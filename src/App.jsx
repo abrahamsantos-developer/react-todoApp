19155051942d532212b2624a11616cc88a8aa686
abrahamsantos-developer <abesantostorres@gmail.com>
2023-10-06 13:51:58 -0600

    added form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./styles.css"
 
-
 export default function App() {
-  return <form className="new-item-form">
-
+  return (
+    <form className="new-item-form">
+    <div className="form-row"></div>
+    <label htmlFor="item">New Item</label>
+    <input type="text" id="item" />
   </form>
+  ) 
 }
